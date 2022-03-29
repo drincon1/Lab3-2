184622ec9ceff464b97821a00193aabeeb6e7382
drincon1 <d.rincon@uniandes.edu.co>
2022-03-29 08:21:15 -0500

    Cambios no relevantes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lab3-2
 Laboratorio 3-2 de Infraestructura de comunicaciones
+# Preparación del ambiente
+# Instrucciones para correr el código
--- servidor.py
+++ servidor.py
@@ -1,1 +1,19 @@
 # Código que representa la aplicación servidor UDP
+
+import socket
+IP = 'localhost'
+PORT = 7000
+
+
+def print_servidor(mjs):
+    print(f"[SERVIDOR] {mjs}")
+
+
+def main():
+    print_servidor("Hola")
+
+
+
+
+if __name__ == '__main__':
+    main()
