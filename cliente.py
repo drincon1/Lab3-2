e3d9119e97f005499437da23ec43dad22156aa9e
drincon1 <d.rincon@uniandes.edu.co>
2022-04-04 17:19:57 -0500

    Falta cambiar la IP del servidor y el cliente

--- cliente.py
+++ cliente.py
@@ -1,1 +1,91 @@
 # Código que representa la aplicación cliente
+from re import T
+import socket
+import os
+import threading
+from datetime import datetime
+import time
+
+# ATRIBUTOS
+IP = 'localhost'
+PORT = 7000
+num_clientes = 0
+tam_archivo = 0
+ruta_archivo = ""
+# MÉTODOS
+def thread_function(server_address):
+    tName = threading.current_thread().getName()
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    cliente.setblocking(1)
+
+    # Genera conexión con el servidor
+    data = "Hola"
+    cliente.sendto(data.encode('utf-8'), server_address)
+    pcliente(tName, f"Comenzando conexión con el servidor que está en el puerto {server_address[1]}")
+    pcliente(tName, f"Conectado por el puerto: {cliente.getsockname()[1]}") 
+    ruta = f"ArchivosRecibidos/{tName}-Prueba-{num_clientes}.txt"
+    archivo = open(ruta, 'wb')
+
+    buffSize = 8192
+    starttime = time.time()
+    lasttime = starttime
+    paquete, server_address = cliente.recvfrom(buffSize)
+    pcliente(tName,'Recibiendo paquetes')
+    
+    try:
+        while(paquete):
+            archivo.write(paquete)
+            cliente.settimeout(5)
+            paquete, server_address = cliente.recvfrom(buffSize)
+
+    finally:
+        pcliente(tName,'Se ha finalizado el proceso de recibiento de paquetes')
+        laptime = round((time.time() - lasttime), 2)
+        archivo.close()
+        cliente.close()
+        
+    tam_arch_recibio = os.stat(ruta).st_size 
+    now = datetime.today()
+    ruta_log = f'Logs/{tName}-{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}-{now.second}.txt'
+    exitoso = ""
+    if tam_arch_recibio == tam_archivo:
+        exitoso = "SI"
+    else:
+        exitoso = "NO"
+        
+    file = open(ruta_log, "w")
+    file.write(f'Nombre del archivo: {ruta_archivo[-9:]}\nTamaño: {ruta_archivo[-9:-4]}\nCliente: {tName}\nTiempo: {laptime}\nBytes esperados: {tam_archivo} B\nBytes recibidos: {tam_arch_recibio} B\nEntrega exitosa: {exitoso}')
+    file.close()
+    
+
+
+def pcliente(tName, msj):
+    print(f'[{tName}] {msj}')
+
+
+def main():
+    global num_clientes
+    global tam_archivo
+    global ruta_archivo
+    server_address = (IP, PORT)
+    # CONFIGURACIÓN DE LA APLICACIÓN CLIENTE
+    config = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    data = "CONFIG"
+    config.sendto(data.encode('utf-8'), server_address)
+
+    data = config.recvfrom(1024)
+    info = data[0].decode('utf-8').split('@')
+    ruta_archivo = info[0]
+    num_clientes = int(info[1])
+    tam_archivo = int(info[2])
+    pcliente('APP', f'Ruta archivo: {ruta_archivo}')
+    pcliente('APP', f'Número clientes: {num_clientes}')
+    # FIN CONFIGURACIÓN DE LA APLICACIÓN CLIENTE
+
+    for i in range(1, num_clientes+1):
+        cliente_t = threading.Thread(target=thread_function, name="Cliente"+str(i), args=(server_address,))  # noqa
+        cliente_t.start()
+
+
+if __name__ == '__main__':
+    main()
--- servidor.py
+++ servidor.py
@@ -1,16 +1,120 @@
-# Código que representa la aplicación servidor UDP
-
+# Código que representa la aplicación servidor UDP
 import socket
+import time
+import os
+import sys
+import threading
+from datetime import datetime
+import time
+
 IP = 'localhost'
 PORT = 7000
+ruta_archivo = ""
+barrier = None
+servidor = None
 
 
-def print_servidor(mjs):
-    print(f"[SERVIDOR] {mjs}")
+def thread_function(client_address):
+    tName = threading.current_thread().getName()
+    pservidor(f'El {tName} se ha conectado en el puerto {client_address[1]}')
+    # pservidor(f'Se enviará al {tName} el archivo en la ruta: {ruta_archivo}')
+    pservidor(f'{tName} esperando')
+    barrier.wait()
+
+    buffSize = 8192
+    c = 0
+    sizeS = os.stat(ruta_archivo)
+    sizeSS = sizeS.st_size  # tamaño en bytes
+    NumS = int(sizeSS / buffSize)
+    NumS = NumS + 1
+
+    check = int(NumS)
+    archivo = open(ruta_archivo, "rb")
+    pservidor(f'Inicio de envio de paquetes al cliente {tName}')
+    aviso = True
+    starttime = time.time()
+    lasttime = starttime
+    
+    
+    while check != 0:
+        paquete = archivo.read(buffSize)
+        servidor.sendto(paquete, client_address)
+        c += 1
+        check -= 1
+        porcentaje = (c/NumS)*100 // 1
+        if porcentaje == 50 and aviso == True:
+            pservidor(f'Se han enviado {porcentaje}% paquetes al cliente {tName}')
+            aviso = False
+            
+    laptime = round((time.time() - lasttime), 2)       
+    pservidor(f'Todo los paquetes enviados a {tName}')
+    archivo.close()
+    
+    now = datetime.today()
+    ruta = f'Logs/S-{tName}-{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}-{now.second}.txt'
+    file = open(ruta, "w")
+    file.write(f'Nombre del archivo: {ruta_archivo[-9:]}\nTamaño: {ruta_archivo[-9:-4]}\nCliente: {tName}\nTiempo: {laptime}')
+    file.close()
+    
+
+def pservidor(msj):
+    print(f"[SERVIDOR] {msj}")
 
 
 def main():
-    print_servidor("Hola")
+    # CONFIGURACIÓN DE LA APLICACIÓN SERVIDOR
+    global ruta_archivo
+    global barrier
+    global servidor
+    # Definir con cuál archivo se trabajará
+    res_archivo = input('¿Cual archivo desea recibir sus clientes?\n[1] 100 MB\n[2] 250 MB\n')
+    # if para establecer la ruta
+    if int(res_archivo) == 1:
+        ruta_archivo = "archivos_servidor/100MB.txt"
+
+    elif int(res_archivo) == 2:
+        ruta_archivo = "archivos_servidor/250MB.txt"
+    else:
+        ruta_archivo = "archivos_servidor/prueba.txt"
+
+
+    # Cuántos clientes se manejaran
+    num_clientes  = int(input('¿Cuántos clientes quiere conectados?\nOpciones válidas: 1,5,10\n'))
+
+    barrier = threading.Barrier(num_clientes)
+    size = os.stat(ruta_archivo)
+    sizeSS = size.st_size
+    pservidor(f'Tamaño del archivo en bytes: {str(sizeSS)}')
+    pservidor(f'El número de paquetes serán: {sizeSS/64000}')
+
+    # CREACIÓN E INICIALIZACIÓN DEL SERVIDOR
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    pservidor('Inicializado')
+    servidor.bind((IP, PORT))
+    pservidor('Esperando conexiones')
+    data = servidor.recvfrom(1024)
+
+    info = data[0].decode('utf-8')
+
+    if info == "CONFIG":
+        pservidor('Se ha recibido la solicitud para configurar la aplicación CLIENTE')
+        servidor.sendto(f'{ruta_archivo}@{num_clientes}@{sizeSS}'.encode('utf-8'),data[1])
+        pservidor(f'Se ha enviado la información para configurar la aplicación CLIENTE a través de: {data[1]}')
+    else:
+        pservidor(f'No se ha recibido la información correcta\nSe ha recibido: {data}')
+
+    # FIN CONFIGURACIÓN DE LA APLICACIÓN SERVIDOR
+
+    i = 1
+    while True:
+        data, client_address = servidor.recvfrom(1024)
+        cliente = threading.Thread(target=thread_function, name="Cliente "+str(i),args=(client_address,))
+        cliente.start()
+        i=i+1
+
+
+    servidor.close()
+    #pservidor('Conexiones cerradas')
 
 
 
